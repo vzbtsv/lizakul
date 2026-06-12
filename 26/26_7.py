afda3ee4101fc4e5607214c25b702b33804c5a02
vladimir <zubtsov.official@gmail.com>
2026-06-12 17:48:24 +0700

    1

--- 26/26_7.py
+++ 26/26_7.py
@@ -1,32 +1,40 @@
-
-
 f = open("26_2254.txt").readlines()
 
 
 n, val = list(map(int, f[0].split()))
 
 
-data = sorted([[int(i) if i not in "QZ" else i for i in x.split() ] for x in f[1:]], key=lambda x: (x[1], x[0]))
+data = sorted([[int(i) if i not in "QZ" else i for i in x.split()] for x in f[1:]], key=lambda x: (x[1], x[0]))
 res = []
 
+
 for x in data:
-    price, state = x
-    if val - price >= 0:
+    price, type_of_product = x
+
+    if len(res) < 95:
         res.append(x)
         val -= price
 
     else:
+        break
 
-
-print(len(res))
 print(res)
+zets = list(filter(lambda x: x[1] == "Z", data))
+
+
+for i in zets:
+    if val >= 0:
+        break
+
+    exp = res.pop(-1)
+    res = [i] + res
+    val += (exp[0] - i[0])
+
+
+print(res)
+print(len(res))
+ques = list(filter(lambda x: x[1] == "Q", res))
+zets = list(filter(lambda x: x[1] == "Z", res))
+
+print(len(ques))
 print(val)
-
-
-
-
-
-
-
-
-
